65003091520b42b6a34e4b678f20a58341e4cc09
dwoo27 <dwooed27@gmail.com>
2026-03-14 03:52:28 -0700

    begin session

--- 210-lab-22.cpp
+++ 210-lab-22.cpp
@@ -1,20 +1,128 @@
-// 210-lab-22.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+class DoublyLinkedList {
+private:
+	struct Node {
+		int data;
+		Node* prev;
+		Node* next;
+		Node(int val, Node* p = nullptr, Node* n = nullptr) {
+			data = val;
+			prev = p;
+			next = n;
+		}
+	};
+	Node* head;
+	Node* tail;
+public:
+	// constructor
+	DoublyLinkedList() { head = nullptr; tail = nullptr; }
+	void push_back(int value) {
+		Node* newNode = new Node(value);
+		if (!tail) // if there's no tail, the list is empty
+			head = tail = newNode;
+		else {
+			tail->next = newNode;
+			newNode->prev = tail;
+			tail = newNode;
+		}
+	}
+	void push_front(int value) {
+		Node* newNode = new Node(value);
+		if (!head) // if there's no head, the list is empty
+			head = tail = newNode;
+		else {
+			newNode->next = head;
+			head->prev = newNode;
+			head = newNode;
+		}
+	}
+	void insert_after(int value, int position) {
+		if (position < 0) {
+			cout << "Position must be >= 0." << endl;
+			return;
+		}
+		Node* newNode = new Node(value);
+		if (!head) {
+			head = tail = newNode;
+			return;
+		}
+		Node* temp = head;
+		for (int i = 0; i < position && temp; ++i)
+			temp = temp->next;
+		if (!temp) {
+			cout << "Position exceeds list size. Node not inserted.\n";
+			delete newNode;
+			return;
+		}
+		newNode->next = temp->next;
+		newNode->prev = temp;
+		if (temp->next)
+			temp->next->prev = newNode;
+		else
+			tail = newNode; // Inserting at the end
+		temp->next = newNode;
+	}
+	void delete_node(int value) {
+		if (!head) return; // Empty list
+		Node* temp = head;
+		while (temp && temp->data != value)
+			temp = temp->next;
+		if (!temp) return; // Value not found
+		if (temp->prev) {
+			temp->prev->next = temp->next;
+		}
+		else {
+			head = temp->next; // Deleting the head
+		}
+		if (temp->next) {
+			temp->next->prev = temp->prev;
+		}
+		else {
+			tail = temp->prev; //Deleting the tail
+		}
+		delete temp;
+	}
+	void print() {
+		Node* current = head;
+		if (!current) return;
+		while (current) {
+			cout << current->data << " ";
+			current = current->next;
+		}
+		cout << endl;
+	}
+	void print_reverse() {
+		Node* current = tail;
+		if (!current) return;
+		while (current) {
+			cout << current->data << " ";
+			current = current->prev;
+		}
+		cout << endl;
+	}
+	~DoublyLinkedList() {
+		while (head) {
+			Node* temp = head;
+			head = head->next;
+			delete temp;
+		}
+	}
+};
+// Driver program
+int main() {
+	DoublyLinkedList list;
+	int size = rand() % (MAX_LS - MIN_LS + 1) + MIN_LS;
+	for (int i = 0; i < size; ++i)
+		list.push_back(rand() % (MAX_NR - MIN_NR + 1) + MIN_NR);
+	cout << "List forward: ";
+	list.print();
+	cout << "List backward: ";
+	list.print_reverse();
+	cout << "Deleting list, then trying to print.\n";
+	list.~DoublyLinkedList();
+	cout << "List forward: ";
+	list.print();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
